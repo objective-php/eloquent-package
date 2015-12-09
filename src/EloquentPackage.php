c97d7aa89721331464248772bd8d9912768586ad
Gauthier Delamarre <gauthier@objective-php.org>
2015-12-09 16:54:24 +0100

    Read configuration and bootstrap Eloquent

--- src/EloquentPackage.php
+++ src/EloquentPackage.php
@@ -1,15 +1,39 @@
 <?php
 
-    namespace ObjectivePHP\Package;
+    namespace ObjectivePHP\EloquentPackage;
 
 
     use ObjectivePHP\Application\ApplicationInterface;
+    use Illuminate\Database\Capsule\Manager as CapsuleManager;
 
     class EloquentPackage
     {
 
         public function __invoke(ApplicationInterface $app)
         {
+            $app->on('bootstrap')->plug([$this, 'bootstrapEloquent']);
+        }
+
+
+        public function bootstrapEloquent(ApplicationInterface $app)
+        {
+            $config = $app->getConfig()->eloquent->toArray();
+
+            if(!$config)
+            {
+                // eloquent has not been configured
+                return null;
+            }
+
+            $capsuleManager = new CapsuleManager();
+
+            $capsuleManager->addConnection($config);
+
+            $capsuleManager->setAsGlobal();
+            $capsuleManager->bootEloquent();
+
+            // register the capsule manager as service
+            $app->getServicesFactory()->registerService(['id' => 'eloquent.capsule', 'instance' => $capsuleManager]);
 
         }
 
