e2ab1809644c38d9d7ca4c6b11e8478e1d3408ba
Gauthier Delamarre <gauthier@objective-php.org>
2015-12-10 10:56:59 +0100

    Register Eloquent Capsule as service by default

--- src/EloquentPackage.php
+++ src/EloquentPackage.php
@@ -6,25 +6,41 @@
     use ObjectivePHP\Application\ApplicationInterface;
     use Illuminate\Database\Capsule\Manager as CapsuleManager;
 
+    /**
+     * Class EloquentPackage
+     *
+     * @package ObjectivePHP\EloquentPackage
+     */
     class EloquentPackage
     {
 
+        /**
+         * @param ApplicationInterface $app
+         */
         public function __invoke(ApplicationInterface $app)
         {
             $app->on('bootstrap')->plug([$this, 'bootstrapEloquent']);
         }
 
-
+        /**
+         * @param ApplicationInterface $app
+         *
+         * @return null
+         */
         public function bootstrapEloquent(ApplicationInterface $app)
         {
             $config = $app->getConfig()->eloquent->toArray();
 
-            if(!$config)
+            // add default values
+            $config += ['charset' => 'utf8', 'collation' => 'utf8_unicode_ci'];
+
+
+            if (!$config)
             {
                 // eloquent has not been configured
                 return null;
             }
-
+            
             $capsuleManager = new CapsuleManager();
 
             $capsuleManager->addConnection($config);
